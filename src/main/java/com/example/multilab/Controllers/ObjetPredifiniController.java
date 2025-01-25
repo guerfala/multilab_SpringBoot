cbfca1028125930d8318e4884653a117f6c8a481
guerfala <mohamedtaher.guerfala@esprit.tn>
2025-01-25 08:48:17 +0100

    CRUD objet mission

--- src/main/java/com/example/multilab/Controllers/ObjetPredifiniController.java
+++ src/main/java/com/example/multilab/Controllers/ObjetPredifiniController.java
@@ -7,23 +7,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/objets")
 @CrossOrigin(origins = "*")
 public class ObjetPredifiniController {
-
     @Autowired
     private ObjetPredifiniRepo objetPredifiniRepo;
 
     @PostMapping("/admin/add-objet-predifini")
     public ResponseEntity<ObjetPredifini> addObjetPredifini(@RequestBody ObjetPredifini objetPredifini) {
-        ObjetPredifini savedObjet = objetPredifiniRepo.save(objetPredifini);
-        return ResponseEntity.ok(savedObjet);
+        return ResponseEntity.ok(objetPredifiniRepo.save(objetPredifini));
     }
 
     @GetMapping
     public List<ObjetPredifini> getAll() {
         return objetPredifiniRepo.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteObjet(@PathVariable int id) {
+        objetPredifiniRepo.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ObjetPredifini> updateObjet(@PathVariable int id, @RequestBody ObjetPredifini objetDetails) {
+        Optional<ObjetPredifini> optionalObjet = objetPredifiniRepo.findById(id);
+        if (optionalObjet.isPresent()) {
+            ObjetPredifini existingObjet = optionalObjet.get();
+            existingObjet.setNom(objetDetails.getNom());
+            ObjetPredifini updatedObjet = objetPredifiniRepo.save(existingObjet);
+            return ResponseEntity.ok(updatedObjet);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
